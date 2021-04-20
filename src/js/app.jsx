e93d59504b3df4ad88c351d1808f7362e5128531
Josiah Swab <josiahjswab@gmail.com>
2021-04-20 09:08:55 -0700

    Fin

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 
 export default class App extends React.Component {
-  // your Javascript goes here
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       balance: 0,
@@ -13,16 +12,24 @@ export default class App extends React.Component {
     this.calculate = this.calculate.bind(this);
   }
 
-  changeInput(object) { //in online examples object is listed as event and wanted to see if the code would break if I changed it. As long as the same var name appears as a parameter in the function it seems to work just fine.
-    this.setState({[object.target.name] : [object.target.value]})
+  changeInput(object) {
+    // in online examples object is listed as event and wanted to see
+    // if the code would break if I changed it.
+    // As long as the same var name appears as a parameter in the
+    // function it seems to work just fine.
+    this.setState({ [object.target.name]: [object.target.value] });
   }
 
-  calculate(){
-    let rate = (this.state.rate)/100/12;
-    let term = (this.state.term) * 12;
-    let balance = (this.state.balance);
-    let monthlyPayment = (balance*(rate*Math.pow((1+rate ),term )/(Math.pow(1+rate,term)-1))).toFixed(2);
-    this.setState({submit: monthlyPayment});
+  calculate() {
+    const rate = (this.state.rate) / 100 / 12;
+    const term = (this.state.term) * 12;
+    const balance = (this.state.balance);
+    const monthlyPayment = (
+      balance * 
+      (rate * Math.pow((1 + rate), term) / 
+      (Math.pow(1 + rate, term) - 1))).toFixed(2);
+    this.setState({ submit: monthlyPayment });
+    console.log(`Rate :${rate} \n\rTerm: ${term} \n\rBalance: ${balance}`)
   }
 
   render() {
@@ -30,15 +37,15 @@ export default class App extends React.Component {
       <div className='container'>
         <div name='input' id='input'>
           <h3>Mortgage Calculator</h3>
-          <input name='balance' type='number' value={this.state.balance} onChange={this.changeInput}/>
-          <input name='rate' type="number" step='0.01' value={this.state.rate} onChange={this.changeInput}/>
-          <select name="term" value={this.state.term} onChange={this.changeInput}>
+          <input name='balance' type='number' value={ this.state.balance } onChange={ this.changeInput }/>
+          <input name='rate' type="number" step='0.01' value={ this.state.rate } onChange={ this.changeInput } />
+          <select name="term" value={ this.state.term } onChange={ this.changeInput }>
             <option value="15">15</option>
             <option value="30">30</option>
           </select>
-          <button name='submit' onClick={this.calculate}>submit</button>
+          <button name='submit' onClick={ this.calculate }>submit</button>
         </div>
-        <div id='output' name='output'> ${this.state.submit} is your payment. </div> 
+        <div id='output' name='output'> ${this.state.submit} is your payment.</div>
       </div>
     );
   }
